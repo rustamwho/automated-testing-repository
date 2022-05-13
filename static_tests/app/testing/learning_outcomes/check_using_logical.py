5c01553ab897946dbee22ac1bb3552ac65ee94a8
rustamwho <rustamwho@gmail.com>
2022-05-13 17:45:59 +0300

    Checking for 'using logical operations' LearningOutcome

---
+++ static_tests/app/testing/learning_outcomes/check_using_logical.py
@@ -0,0 +1,72 @@
+from testing.models import LearningOutcome, Recommendation
+from testing.helpers import search_with_func
+from testing import parsers
+
+
+def five(file_list: list):
+    if search_with_func(
+            file_list,
+            parsers.is_exist_multiple_logical_ops_in_one_expression):
+        return []
+    return [
+        Recommendation(
+            name='Использует сложные логические операции',
+            task='Модуль 1. Задача 1.'
+        )
+    ]
+
+
+def four(file_list: list):
+    recommendations = []
+    for operator, task in (('not', 'Модуль 10. Задача 2.'),
+                           ('and', 'Модуль 10. Задача 2.'),
+                           ('or', 'Модуль 1. Задача 1.')):
+        if search_with_func(file_list, pattern=operator,
+                            func=parsers.is_exists_pattern):
+            continue
+        recommendations.append(
+            Recommendation(
+                name=f'Использует логическую операцию: {operator}',
+                task=task
+            )
+        )
+    return recommendations
+
+
+def three(file_list: list):
+    recommendations = []
+    for operator, task in (('<|>', 'Модуль 4. Задача 1.'),
+                           ('>=|<=', 'Модуль 4. Задача 1.'),
+                           ('!=', 'Модуль 7. Задача 1.'),
+                           ('==', 'Модуль 6. Задача 1.')):
+        if search_with_func(file_list, pattern=operator,
+                            func=parsers.is_exists_pattern):
+            continue
+        recommendations.append(
+            Recommendation(
+                name=f'Использует логический оператор: {operator}',
+                task=task
+            )
+        )
+    return recommendations
+
+
+def get_learning_outcome(file_list):
+    learning_outcome = LearningOutcome(
+        name='Использует логические операции',
+    )
+
+    if not file_list:
+        learning_outcome.score = 2
+        return learning_outcome
+
+    for func_score in (five,):  # , four, three):
+        recommendations = func_score(file_list)
+        # If all the criteria of this level are passed
+        if not recommendations:
+            break
+        # If recommendations are exists, add to the learning outcome
+        for recommendation in recommendations:
+            learning_outcome.recommendations.append(recommendation)
+        learning_outcome.score -= 1
+    return learning_outcome
--- static_tests/app/testing/parsers.py
+++ static_tests/app/testing/parsers.py
@@ -4,6 +4,7 @@ import re
 def is_exists_overload_arithmetic(file_name: str, operator: str) -> bool:
     """
     Search overloading arithmetic operators.
+
     :param file_name: str of the file for matching
     :param operator: operator
     :return: True if find, else - False
@@ -21,7 +22,7 @@ def is_exists_overload_arithmetic(file_name: str, operator: str) -> bool:
 def is_exists_pattern(file_name: str, pattern: str) -> bool:
     """
     Search pattern in file.
-    Final patter for matching: fr'^\s*\w+\s*\{pattern}\s*\w+'.
+    Final pattern for matching: fr'^\s*\w+\s*\{pattern}\s*\w+'.
 
     :param file_name: str of the file for matching
     :param pattern: pattern for searching
@@ -30,6 +31,26 @@ def is_exists_pattern(file_name: str, pattern: str) -> bool:
     # ^\s* - строка начинается с 0 или более пробелов
     # \w+ - любой символ, котороый может быть переменной (буквы, цифры и _)
     pattern = fr'^\s*\w+\s*\{pattern}\s*\w+'
+    with open(file_name, 'r') as file:
+        match = re.findall(pattern, file.read(), flags=re.MULTILINE)
+        if match:
+            return True
+    return False
+
+
+def is_exist_multiple_logical_ops_in_one_expression(file_name: str) -> bool:
+    """
+    Search using multiple logical operations in one expression in <file_name>.
+
+    :param file_name: str of the file for matching
+    :return: True if multiple logical operations in one expression are exist,
+    else - False
+    """
+    # ^[^#\'\"\n]* - строка начинается с любого символа кроме этих
+    # (?:or|and|not|>|<|>=|<=|!=|==) - комбинация из двух операторов
+    # (между ними любые символы кроме переноса строки .+)
+    pattern = (r'^[^#\'\"\n]*(?:or|and|not|>|<|>=|<=|!=|==).+'
+               r'(?:or|and|not|>|<|>=|<=|!=|==)[^\'\"\n]+$')
     with open(file_name, 'r') as file:
         match = re.findall(pattern, file.read(), flags=re.MULTILINE)
         if match:
