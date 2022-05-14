415af1bcdea41ded84e7ece6648b859160f15e8a
rustamwho <rustamwho@gmail.com>
2022-05-14 20:07:09 +0300

    Checking for 'working with strings' LearningOutcome

---
+++ static_tests/app/testing/learning_outcomes/check_working_str.py
@@ -0,0 +1,100 @@
+from testing.models import LearningOutcome, Recommendation
+from testing import parsers
+from testing.helpers import search_with_func
+
+
+def five(file_list: list):
+    if search_with_func(
+            file_list,
+            func=parsers.is_using_function, function_name='ord|chr'):
+        return []
+    return [
+        Recommendation(
+            name='Понимает как хранится строка в памяти: использует ord() или'
+                 'chr()',
+            task='Модуль 4. Задача 1.'
+        )
+    ]
+
+
+def four(file_list: list):
+    recommendations = []
+    for method_name, task in (('replace', 'Модуль 10. Задача 8.'),
+                              ('find', 'Модуль 4. Задача 3.'),
+                              ('lower|upper', 'Модуль 4. Задача 2.'),
+                              ('split|join', 'Модуль 6. Задача 2.')):
+        if search_with_func(file_list, func=parsers.is_using_method,
+                            method_name=method_name):
+            continue
+        recommendations.append(
+            Recommendation(
+                name=f'Использует методы: {method_name}',
+                task=task
+            )
+        )
+    if not search_with_func(file_list, func=parsers.is_exist_slices):
+        recommendations.append(
+            Recommendation(
+                name='Умеет брать срезы',
+                task='Модуль 10. Задача 5.'
+            )
+        )
+    return recommendations
+
+
+def three(file_list: list):
+    recommendations = []
+    if not search_with_func(file_list,
+                            func=parsers.is_exists_str_iteration):
+        recommendations.append(
+            Recommendation(
+                name='Использует итерирование строк',
+                task='Модуль 4. Задача 1.'
+            )
+        )
+    if not search_with_func(file_list,
+                            func=parsers.is_exists_str_indexation):
+        recommendations.append(
+            Recommendation(
+                name='Использует индексацию',
+                task='Модуль 4. Задача 1.'
+            )
+        )
+    if not search_with_func(file_list,
+                            func=parsers.is_exists_str_length):
+        recommendations.append(
+            Recommendation(
+                name='Умеет узнавать длину строки',
+                task='Модуль 10. Задача 5.'
+            )
+        )
+    if not search_with_func(file_list,
+                            func=parsers.is_exists_creating_str):
+        recommendations.append(
+            Recommendation(
+                name='Умеет создавать строку',
+                task='Модуль 4. Задача 1.'
+            )
+        )
+    return recommendations
+
+
+def get_learning_outcome(file_list):
+    learning_outcome = LearningOutcome(
+        name='Умеет работать со строками',
+    )
+
+    if not file_list:
+        learning_outcome.score = 2
+        return learning_outcome
+
+    for func_score in (five, four, three):
+        recommendations = func_score(file_list)
+        # If all the criteria of this level are passed
+        if not recommendations:
+            break
+        # If recommendations are exists, add to the learning outcome
+        for recommendation in recommendations:
+            learning_outcome.recommendations.append(recommendation)
+        learning_outcome.score -= 1
+    return learning_outcome
--- static_tests/app/testing/parsers.py
+++ static_tests/app/testing/parsers.py
@@ -56,3 +56,110 @@ def is_exist_multiple_logical_ops_in_one_expression(file_name: str) -> bool:
         if match:
             return True
     return False
+
+
+def is_using_function(file_name: str, function_name: str) -> bool:
+    """
+    Search using function with <function_name> in <file_name>.
+
+    :param file_name: str of the file for matching
+    :param function_name: name of the function to search
+    :return: True if <function_name> function is using, else - False
+    """
+    pattern = fr'^[^#\'\"\n]*{function_name}\(.+\).*$'
+    with open(file_name, 'r') as file:
+        match = re.findall(pattern, file.read(), flags=re.MULTILINE)
+        if match:
+            return True
+    return False
+
+
+def is_using_method(file_name: str, method_name: str) -> bool:
+    """
+    Search using <method_name> method in <file_name> [.method_name()].
+
+    :param file_name: str of the file for matching
+    :param method_name: name of the method to search
+    :return:True if <method_name> method is using, else - False
+    """
+    pattern = fr'^[^#\'\"\n]*\w+.{method_name}\(.+\).*$'
+    with open(file_name, 'r') as file:
+        match = re.findall(pattern, file.read(), flags=re.MULTILINE)
+        if match:
+            return True
+    return False
+
+
+def is_exist_slices(file_name: str) -> bool:
+    """
+    Search using string slices.
+
+    :param file_name:str of the file for matching
+    :return:True if slices are exist, else - False
+    """
+    pattern = fr'^[^#\'\"\n]*\w+\[\d*:\d*].*$'
+    with open(file_name, 'r') as file:
+        match = re.findall(pattern, file.read(), flags=re.MULTILINE)
+        if match:
+            return True
+    return False
+
+
+def is_exists_str_iteration(file_name: str) -> bool:
+    """
+    Search using string iteration.
+
+    :param file_name:str of the file for matching
+    :return:True if using string iteration is exists, else - False
+    """
+    pattern = fr'^[^#\'\"\n]*for \w+ in range\(len\(\w+\)\).*$'
+    with open(file_name, 'r') as file:
+        match = re.findall(pattern, file.read(), flags=re.MULTILINE)
+        if match:
+            return True
+    return False
+
+
+def is_exists_str_indexation(file_name: str) -> bool:
+    """
+    Search using string indexation.
+
+    :param file_name: str of the file for matching
+    :return: True if using string indexation is exists, else - False
+    """
+    pattern = fr'^[^#\'\"\n]*(\w+\[\d+]).*$'
+    with open(file_name, 'r') as file:
+        match = re.findall(pattern, file.read(), flags=re.MULTILINE)
+        if match:
+            return True
+    return False
+
+
+def is_exists_str_length(file_name: str) -> bool:
+    """
+    Search using len().
+
+    :param file_name: str of the file for matching
+    :return: True if using len() is exists, else - False
+    """
+    pattern = fr'^[^#\'\"\n]*(len\(\w+\)).*$'
+    with open(file_name, 'r') as file:
+        match = re.findall(pattern, file.read(), flags=re.MULTILINE)
+        if match:
+            return True
+    return False
+
+
+def is_exists_creating_str(file_name: str) -> bool:
+    """
+    Search creating string. With input or ' or ".
+
+    :param file_name: str of the file for matching
+    :return: True if creating string is exists, else - False
+    """
+    pattern = fr'^[^#\'\"\n]*\w+ = (input\(|\'|\").*$'
+    with open(file_name, 'r') as file:
+        match = re.findall(pattern, file.read(), flags=re.MULTILINE)
+        if match:
+            return True
+    return False
