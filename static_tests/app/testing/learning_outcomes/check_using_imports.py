1ca575a506f077f686df63282bc43882b7077537
rustamwho <rustamwho@gmail.com>
2022-05-18 17:09:43 +0300

    Checking 'using imports' LearningOutcome

---
+++ static_tests/app/testing/learning_outcomes/check_using_imports.py
@@ -0,0 +1,60 @@
+from testing.models import LearningOutcome, Recommendation
+from testing import parsers
+from testing.helpers import search_with_func
+from testing.utils import get_module_info
+
+
+def five(file_list: list):
+    if not search_with_func(file_list, func=parsers.is_exists_import_alias):
+        return [Recommendation(
+            name='Умеет использовать псведонимы',
+            task='Модуль 10. Задача 5.'
+        )]
+    return []
+
+
+def four(file_list: list):
+    if not search_with_func(file_list, func=parsers.is_exists_import_from):
+        return [Recommendation(
+            name='Использует конструкцию from',
+            task='Модуль 10. Задача 5.'
+        )]
+    return []
+
+
+def three(file_list: list):
+    is_using_imports = False
+    for file in file_list:
+        module_info = get_module_info(file)
+        if module_info.imports:
+            is_using_imports = True
+            break
+
+    if is_using_imports:
+        return []
+
+    return [Recommendation(
+        name='Умеет импортировать модули',
+        task='Модуль 8. Задача 1.'
+    )]
+
+
+def get_learning_outcome(file_list):
+    learning_outcome = LearningOutcome(
+        name='Умеет работать с модулями',
+    )
+
+    if not file_list:
+        learning_outcome.score = 2
+        return learning_outcome
+
+    for func_score in (five, four, three):
+        recommendations = func_score(file_list)
+        # If all the criteria of this level are passed
+        if not recommendations:
+            break
+        # If recommendations are exists, add to the learning outcome
+        for recommendation in recommendations:
+            learning_outcome.recommendations.append(recommendation)
+        learning_outcome.score -= 1
+    return learning_outcome
