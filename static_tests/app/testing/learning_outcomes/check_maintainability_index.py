8927e0d870257ae31d8570960762acf85e38959f
rustamwho <rustamwho@gmail.com>
2022-05-19 16:34:22 +0300

    Checking 'maintainability index' LearningOutcome

---
+++ static_tests/app/testing/learning_outcomes/check_maintainability_index.py
@@ -0,0 +1,24 @@
+from testing.models import LearningOutcome
+from testing.utils import get_maintainability_indexes
+from testing.helpers import get_score_from_maintainability_indexes
+
+
+def get_learning_outcome(file_list):
+    learning_outcome = LearningOutcome(
+        name=('Понимает сложность функций и методов класса, пишет код с '
+              'наименьшей сложностью (индекс поддерживаемости)'),
+    )
+
+    if not file_list:
+        learning_outcome.score = 2
+        return learning_outcome
+
+    maintainability_indexes = get_maintainability_indexes(file_list)
+    if not maintainability_indexes:
+        learning_outcome.score = 2
+        return learning_outcome
+    score = get_score_from_maintainability_indexes(maintainability_indexes)
+
+    learning_outcome.score = score
+
+    return learning_outcome
