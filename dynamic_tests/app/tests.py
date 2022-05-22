f3c0d682ac578e0df9d4c55cdf378c39555e9935
rustamwho <rustamwho@gmail.com>
2022-05-22 20:13:34 +0300

    - Tests for module_6/task_5 - Fix input with prompt checking

--- dynamic_tests/app/tests.py
+++ dynamic_tests/app/tests.py
@@ -21,6 +21,7 @@ TEST_FILE_TEST_CASE = {
     'module_6/task_2_matrix.py': tc.TEST_CASE_module_6_task_2,
     'module_6/task_3_google_search.py': tc.TEST_CASE_module_6_task_3,
     'module_6/task_4_chunking.py': tc.TEST_CASE_module_6_task_4,
+    'module_6/task_5_lists.py': tc.TEST_CASE_module_6_task_5,
 }
 
 
