8fb428c16fc37383195fc72022ca5e8ea67ab4b4
rustamwho <rustamwho@gmail.com>
2022-05-23 17:25:09 +0300

    Tests for module_7/task_2

--- dynamic_tests/app/tests.py
+++ dynamic_tests/app/tests.py
@@ -23,6 +23,7 @@ TEST_FILE_TEST_CASE = {
     'module_6/task_4_chunking.py': tc.TEST_CASE_module_6_task_4,
     'module_6/task_5_lists.py': tc.TEST_CASE_module_6_task_5,
     'module_7/task_1_unique_cities.py': tc.TEST_CASE_module_7_task_1,
+    'module_7/task_2_number_of_matching.py': tc.TEST_CASE_module_7_task_2,
 }
 
 
